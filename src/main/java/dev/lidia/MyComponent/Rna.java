07bec0c2dad194b69901dfa5af6740e8ffbf707e
unknown <lgtablasf5@gmail.com>
2025-03-27 13:24:01 +0100

    test dnaToRna fault

--- src/main/java/dev/lidia/MyComponent/Rna.java
+++ src/main/java/dev/lidia/MyComponent/Rna.java
@@ -2,7 +2,7 @@ package dev.lidia.MyComponent;
 
 public class Rna {
 
-    public static String testInit() {
-        return String.valueOf(1);
+    private static void dnaToRna () {
+        
     }
 }
--- src/test/java/dev/lidia/MyComponent/RnaTest.java
+++ src/test/java/dev/lidia/MyComponent/RnaTest.java
@@ -1,14 +1,22 @@
 package dev.lidia.MyComponent;
 import org.hamcrest.MatcherAssert;
+
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 
 public class RnaTest {
+    
+    private DnaToRna dnaToRna = new DnaToRna(); 
+
     @Test
-    @DisplayName ("first test")
-    public void testInit() {
-        MatcherAssert.assertThat(1, is(1));
+    @DisplayName ("testing dna to rna")
+    public void dnaToRna() {
+        String result = dnaToRna.equalRna ("GCTA");
+        assertThat(result, is(equalTo("CGAU")));
     }
 }
