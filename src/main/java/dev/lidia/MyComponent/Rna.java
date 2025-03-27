141feea4eac30b24b40f95c06f611c3e210d28d2
unknown <lgtablasf5@gmail.com>
2025-03-27 10:45:42 +0100

    firts test succesfull

--- src/main/java/dev/lidia/MyComponent/Rna.java
+++ src/main/java/dev/lidia/MyComponent/Rna.java
@@ -1,5 +1,8 @@
 package dev.lidia.MyComponent;
 
 public class Rna {
-    
+
+    public static String testInit() {
+        return String.valueOf(1);
+    }
 }
--- src/test/java/dev/lidia/MyComponent/RnaTest.java
+++ src/test/java/dev/lidia/MyComponent/RnaTest.java
@@ -1,14 +1,14 @@
 package dev.lidia.MyComponent;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.hamcrest.MatcherAssert;
 import static org.hamcrest.Matchers.is;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 
 public class RnaTest {
     @Test
-    
+    @DisplayName ("first test")
     public void testInit() {
-        MatcherAssert.assertThat(1, is("Rna"));
+        MatcherAssert.assertThat(1, is(1));
     }
 }
